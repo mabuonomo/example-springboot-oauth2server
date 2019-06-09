7d76d3122089bd97eaa43f2d80d7da37cc94008d
Mario Buonomo <ma.buonomo@gmail.com>
2019-06-09 12:55:19 +0200

    first commit

---
+++ src/main/java/it/squaloxp/AuthServer/UserController.java
@@ -0,0 +1,15 @@
+package it.squaloxp.AuthServer;
+ 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+ 
+import java.security.Principal;
+ 
+@RestController
+public class UserController {
+ 
+    @GetMapping("/user/me")
+    public Principal user(Principal principal) {
+        return principal;
+    }
+}
